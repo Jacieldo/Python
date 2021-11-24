3b02700705170f4642153720bfaed07190a8f4ce
Jacieldo Gonçalves <jacieldo@hotmail.com>
2021-11-24 01:56:01 -0300

    Create Desafio 52.py

---
+++ Mundo2/Desafio 52.py
@@ -0,0 +1,13 @@
+print('----->DESAFIO 52<-----')
+num = int(input('Digite um número: '))
+
+cont = 0
+
+for c in range(1, num +1):
+    if num % c == 0:
+        cont += 1
+
+if cont == 2:
+    print('o número {} é um número primo!'.format(num))
+else:
+    print('O número {} não é um número primo!'.format(num))
